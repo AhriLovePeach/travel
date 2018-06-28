2de99a3f2656068315b6759ff6492b097e362747
zhoufei <157690910@qq.com>
2018-06-28 15:56:43 +0800

    ''

--- travle/src/pages/city/components/Alphabet.vue
+++ travle/src/pages/city/components/Alphabet.vue
@@ -12,8 +12,13 @@ export default {
     },
     data() {
         return {
-            touchStatus: false
+            touchStatus: false,
+            startY: 0,
+            timer: null
         };
+    },
+    updated() {
+        this.startY = this.$refs["A"][0].offsetTop;
     },
     computed: {
         // 地域的首字母数组
@@ -53,12 +58,16 @@ export default {
          */
         handleTouchMove(e) {
             if (this.touchStatus) {
-                const startY = this.$refs["A"][0].offsetTop;
-                const touchY = e.targetTouches[0].clientY - 79;
-                const index = Math.floor((touchY - startY) / 20);
-                if (index >= 0 && index < this.letters.length) {
-                    this.$emit("change", this.letters[index]);
+                if (this.timer) {
+                    clearTimeout(this.timer);
                 }
+                this.timer = setTimeout(() => {
+                    const touchY = e.targetTouches[0].clientY - 79;
+                    const index = Math.floor((touchY - this.startY) / 20);
+                    if (index >= 0 && index < this.letters.length) {
+                        this.$emit("change", this.letters[index]);
+                    }
+                }, 16);
             }
         },
         /**
