16a06ac34b4e9ed4fcd2c56ed92511e2a69dc7e5
zhoufei <157690910@qq.com>
2018-06-28 17:01:31 +0800

    '搜索匹配'

--- travle/src/pages/city/components/Search.vue
+++ travle/src/pages/city/components/Search.vue
@@ -1,11 +1,50 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" name="" placeholder="输入城市名或拼音" />
+    <div>
+        <div class="search">
+            <input v-model="keyWorld" class="search-input" type="text" name="" placeholder="输入城市名或拼音" />
+        </div>
+        <div class="search-content">
+            <ul>
+                <li class="search-item" v-for="item of list" :key="item.id">{{item.name}}</li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    name: "CitySearch"
+    name: "CitySearch",
+    props: {
+        cities: Object
+    },
+    data() {
+        return {
+            keyWorld: "",
+            list: [],
+            timer: null
+        };
+    },
+    watch: {
+        keyWorld() {
+            let cities = this.cities;
+            if (this.timer) {
+                clearTimeout(this.timer);
+            }
+            this.timer = setTimeout(() => {
+                const result = [];
+                for (let i in cities) {
+                    cities[i].forEach(value => {
+                        if (
+                            value.spell.indexOf(this.keyWorld) > -1 ||
+                            value.name.indexOf(this.keyWorld) > -1
+                        ) {
+                            result.push(value);
+                        }
+                    });
+                }
+                this.list = result;
+            }, 100);
+        }
+    }
 };
 </script>
 <style lang="stylus" scoped>
@@ -26,5 +65,16 @@ export default {
     text-align: center;
     border-radius: 0.06rem;
     color: #666;
+}
+
+.search-content {
+    z-index: 1;
+    overflow: hidden;
+    position: absolute;
+    top: 1.58rem;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: red;
 }
 </style>
